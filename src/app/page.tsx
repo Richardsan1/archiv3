ff552ff99a14b86ba2a61e437af5c5e0b7243f38
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-05-04 15:45:31 -0300

    chore: Add About page and update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
     return (
-        <main>
-            <h1 className="text-4xl">Bem Vindo ao Archiv3</h1>
-        </main>
+        <div className="flex h-full flex-col items-center justify-center gap-12 p-4 pb-64">
+            <section>
+                <h1 className="text-center text-4xl">
+                    Bem Vindo ao <span>Archiv3</span>
+                </h1>
+                <p className="text-center text-lg">
+                    Aqui você pode arquivar sites, artigos e noticias para sua
+                    preservação histórica.
+                </p>
+            </section>
+            <main className="flex justify-center">
+                <div className="flex flex-col gap-1">
+                    <input
+                        type="text"
+                        className="border border-black p-1"
+                        placeholder="Insira o link aqui"
+                    />
+                    <button className="rounded-lg border border-black bg-black p-1 text-white">
+                        Arquivar
+                    </button>
+                </div>
+            </main>
+        </div>
     )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between p-4">
+            <h1 className="text-4xl font-semibold text-green-500">
+                A<span className="text-black">rchiv</span>3
+            </h1>
+            <nav>
+                <ul className="flex gap-4">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">Sobre</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
