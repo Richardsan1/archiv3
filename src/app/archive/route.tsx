7aa87a2e9801ca90b989515e586a27957de8f0d4
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-05-04 21:13:38 -0300

    chore: Refactor archive route to use async/await for lighthouse upload

--- src/app/archive/route.tsx
+++ src/app/archive/route.tsx
@@ -5,8 +5,12 @@ import { NextRequest } from 'next/server'
 
 export async function GET(req: NextRequest) {
     const link = req.nextUrl.searchParams.get('link') as string
-    GetPage(link).then((file) => {
-        lighthouse.upload(file, process.env.API_KEY_LIGHTHOUSE as string)
+    GetPage(link).then(async (file) => {
+        const response = await lighthouse.upload(
+            file,
+            process.env.API_KEY_LIGHTHOUSE as string
+        )
+        console.log(response.data.Hash)
     })
     redirect('/')
 }
