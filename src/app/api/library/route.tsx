e7b30ed55fb273cf44ca43578f93d49865c622ef
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-05-05 16:41:49 -0300

    chore: Refactor library route to include timestamp in file info

--- src/app/api/library/route.tsx
+++ src/app/api/library/route.tsx
@@ -6,15 +6,18 @@ export async function GET(req: NextRequest) {
         process.env.API_KEY_LIGHTHOUSE as string
     )
     const files = CIDs.data.fileList
-    const filesWithInfo = await files.map(async (file) => {
-        const info = await lighthouse.getFileInfo(file.cid)
+    const filesWithInfo = await Promise.all(
+        files.map(async (file) => {
+            const info = await lighthouse.getFileInfo(file.cid)
 
-        return {
-            id: file.id,
-            cid: file.cid,
-            fileName: info.data.fileName
-        }
-    })
+            return {
+                id: file.id,
+                cid: file.cid,
+                fileName: info.data.fileName,
+                timestamp: file.createdAt
+            }
+        })
+    )
     console.log(CIDs.data.fileList, filesWithInfo)
     return new Response(JSON.stringify(filesWithInfo), {
         headers: { 'content-type': 'application/json' }
